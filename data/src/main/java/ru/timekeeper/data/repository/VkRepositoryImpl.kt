75b150dd87595d2bd2644cfc92793eccdba48b6a
Aydar <aydar710@gmail.com>
2019-04-16 17:07:58 +0300

    fixed

--- data/src/main/java/ru/timekeeper/data/repository/VkRepositoryImpl.kt
+++ data/src/main/java/ru/timekeeper/data/repository/VkRepositoryImpl.kt
@@ -1,7 +1,5 @@
 package ru.timekeeper.data.repository
 
-import io.reactivex.Single
-import io.reactivex.schedulers.Schedulers
 import ru.timekeeper.core.interfaces.VkRepository
 import ru.timekeeper.data.service.VkService
 import javax.inject.Inject
